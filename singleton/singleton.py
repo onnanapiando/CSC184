a2b4c5344f26bafe79c9026d74215838ce1e37a1
onnanapiando <onnapiando@gmail.com>
2014-11-27 06:27:06 +0800

    created a class for creating a thread

--- singleton/singleton.py
+++ singleton/singleton.py
@@ -1,5 +1,24 @@
+import httplib2
+import os
+import re
+import threading
+import urllib
+from urlparse import urlparse, urljoin
+from BeautifulSoup import BeautifulSoup
+
 class Singleton(object):
     def __new__(cls):
       if not hasattr(cls, 'instance'):
         cls.instance = super(Singleton, cls).__new__(cls)
       return cls.instance
+
+class ImageDownloaderThread(threading.Thread):
+    """A thread for downloading images in parallel."""
+    def __init__(self, thread_id, name, counter):
+      threading.Thread.__init__(self)
+      self.name = name
+
+    def run(self):
+      print 'Starting thread ', self.name
+      download_images(self.name)
+      print 'Finished thread ', self.name
