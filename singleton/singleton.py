b6e9f696788833777a6128dc7cab7bfcf36d0804
onnanapiando <onnapiando@gmail.com>
2014-11-27 06:05:39 +0800

    added lines to singleton file

--- singleton/singleton.py
+++ singleton/singleton.py
@@ -1,1 +1,5 @@
-# -*- coding: utf-8 -*-
+class Singleton(object):
+    def __new__(cls):
+      if not hasattr(cls, 'instance'):
+        cls.instance = super(Singleton, cls).__new__(cls)
+      return cls.instance
