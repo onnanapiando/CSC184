d6358a8569942fe8a07b8005aec281c26ebe91cb
onnanapiando <onnapiando@gmail.com>
2014-12-07 15:53:20 +0800

    created RealSubject class

--- proxy/proxy.py
+++ proxy/proxy.py
@@ -9,3 +9,19 @@ class AbstractSubject(object):
 @abstractmethod
 def sort(self, reverse=False):
   pass
+
+class RealSubject(AbstractSubject):
+  """A class for a heavy object which takes a lot of memory
+space and takes some time to instantiate."""
+
+def __init__(self):
+  self.digits = []
+
+  for i in xrange(10000000):
+    self.digits.append(random.random())
+
+  def sort(self, reverse=False):
+    self.digits.sort()
+
+    if reverse:
+      self.digits.reverse()
