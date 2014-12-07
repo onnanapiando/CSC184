4bb48e56907cf93d10bc89b68b3b769f5ef66312
onnanapiando <onnapiando@gmail.com>
2014-12-07 16:50:36 +0800

    added USATimeObserver class

--- observer/observer.py
+++ observer/observer.py
@@ -33,3 +33,11 @@ class Observer(object):
   @abstractmethod
   def notify(self, unix_timestamp):
     pass
+
+class USATimeObserver(Observer):
+  def __init__(self, name):
+    self.name = name
+  
+  def notify(self, unix_timestamp):
+    time = datetime.datetime.fromtimestamp(int(unix_timestamp)).strftime('%Y-%m-%d %I:%M:%S%p') print 'Observer', self.name, 'says:', time
+
